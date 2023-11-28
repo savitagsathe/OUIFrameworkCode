77487af746ff745eaf0bd5e373b0fba66cc8ee19
savitagsathe <savitagsathe@gmail.com>
2023-11-28 11:32:27 +0530

    cart page added

---
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -0,0 +1,15 @@
+package com.qa.opencart.pages;
+
+public class CartPage {
+	
+	public CartPage() {
+		
+		System.out.println("cart page constructor");
+	}
+
+public void addToCart() {
+		
+		System.out.println("add To Cart");
+	}
+
+}
