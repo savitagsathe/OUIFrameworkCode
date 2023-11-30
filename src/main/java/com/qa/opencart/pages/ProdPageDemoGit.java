31ba08688599309df73211e3a7e37cd19708ba8e
savitagsathe <savitagsathe@gmail.com>
2023-11-30 17:11:42 +0530

    latest feature completed

--- src/main/java/com/qa/opencart/pages/ProdPageDemoGit.java
+++ src/main/java/com/qa/opencart/pages/ProdPageDemoGit.java
@@ -5,8 +5,9 @@ public class ProdPageDemoGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("demo1");
-		System.out.println("demo2 by tom");
 
+		System.out.println("demo3 by savita");
+		System.out.println("demo4");
 	}
 
 }
