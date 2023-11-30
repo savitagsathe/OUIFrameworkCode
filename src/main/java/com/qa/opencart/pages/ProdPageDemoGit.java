f8e3c195a7e5646b879c433007c16dee8f295240
savitagsathe <125879049+savitagsathe@users.noreply.github.com>
2023-11-30 16:50:27 +0530

    Update ProdPageDemoGit.java

--- src/main/java/com/qa/opencart/pages/ProdPageDemoGit.java
+++ src/main/java/com/qa/opencart/pages/ProdPageDemoGit.java
@@ -5,6 +5,7 @@ public class ProdPageDemoGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("demo1");
+		System.out.println("demo2 by tom");
 
 	}
 
