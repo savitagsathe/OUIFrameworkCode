4c585e3ad9105db62bbe876e7866e59725b8f30c
savitagsathe <savitagsathe@gmail.com>
2023-11-30 15:43:58 +0530

    product updated code

--- src/main/java/com/qa/opencart/pages/GitProductDemoIgnore.java
+++ src/main/java/com/qa/opencart/pages/GitProductDemoIgnore.java
@@ -4,5 +4,8 @@ public class GitProductDemoIgnore {
 
 	public static void main(String args[]) {
 		System.out.println("product demo");
+		System.out.println("product demo2");
+		System.out.println("product demo 3");
+		
 	}
 }
